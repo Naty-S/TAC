e82a92c4c0e05a27f042003bfe401daaf76c8799
Andrés Ignacio Torres <andresitorresm@gmail.com>
2020-01-21 22:46:57 -0400

    translate operand names to English

--- TACType.hs
+++ TACType.hs
@@ -14,20 +14,20 @@ data (SymEntryCompatible a) => ThreeAddressCode a b = ThreeAddressCode
 data (SymEntryCompatible a) => Operand a b = Variable a | Constant b
 
 data Operation =
-  Suma            |
-  Resta           |
-  Multiplicacion  |
-  Division        |
-  DivisionEntera  |
-  Y               |
-  O               |
-  Modulo          |
-  Mayor           |
-  MayorQue        |
-  Menor           |
-  MenorQue        |
-  Igual           |
-  NoIgual         |
-  Concatenacion   |
+  Add             | -- Addition (2 operands)
+  Substract       | -- Substraciton (2 operands)
+  Multiply        | -- Multiplication (2 operands)
+  Divide          | -- Division (2 operands)
+  IntDivide       | -- Integer Division (2 operands)
+  Modulo          | -- Modulus / Remainder (2 operands)
+  And             | -- And / Conjuction (2 operands)
+  Or              | -- Or / Disjunction (2 operands)
+  Gt              | -- Greater Than (2 operands)
+  Gte             | -- Greater Than or Equal To (2 operands)
+  Lt              | -- Less Than (2 operands)
+  Lte             | -- Less Than or Equal To (2 operands)
+  Equal           | -- Equality (2 operands)
+  NotEqual        | -- Inequality (2 operands)
+  Concat          | -- Concatenation (2 operands)
   AnexoConcat
   deriving (Eq)
