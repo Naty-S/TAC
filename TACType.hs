b176adbbce6a9c47cbf79bd68be2a1deb10f1f71
Naty-S <na7a5cha010@gmail.com>
2020-03-13 08:57:55 -0400

    Otro arreglo para que este como lo tienen todos

--- TACType.hs
+++ TACType.hs
@@ -5,7 +5,7 @@ class SymEntryCompatible a where
   getSymID :: a -> String
 
 
-data (SymEntryCompatible a) => ThreeAddressCode a b = TACC
+data (SymEntryCompatible a) => ThreeAddressCode a b = ThreeAddressCode
   { tacOperand :: Operation,
     tacLvalue  :: Maybe (Operand a b),
     tacRvalue1 :: Maybe (Operand a b),
@@ -15,58 +15,58 @@ data (SymEntryCompatible a) => ThreeAddressCode a b = TACC
 
 
 instance (SymEntryCompatible a, Show a, Show b) => Show (ThreeAddressCode a b) where
-  show (TACC Assign (Just x) (Just y) Nothing)  = "\t" ++ show x ++ " := " ++ show y
+  show (ThreeAddressCode Assign (Just x) (Just y) Nothing)  = "\t" ++ show x ++ " := " ++ show y
 -- For null pointer?
-  show (TACC Assign (Just x) Nothing Nothing)   = "\t" ++ show x ++ " := 0"
+  show (ThreeAddressCode Assign (Just x) Nothing Nothing)   = "\t" ++ show x ++ " := 0"
 -- Arithmetic
-  show (TACC Add (Just x) (Just y) (Just z))    = "\t" ++ show x ++ " := " ++ show y ++ " + " ++ show z
-  show (TACC Sub (Just x) (Just y) (Just z))    = "\t" ++ show x ++ " := " ++ show y ++ " - " ++ show z
-  show (TACC Minus (Just x) (Just y) Nothing)   = "\t" ++ show x ++ " := -" ++ show y 
-  show (TACC Mult (Just x) (Just y) (Just z))   = "\t" ++ show x ++ " := " ++ show y ++ " * " ++ show z
-  show (TACC Div (Just x) (Just y) (Just z))    = "\t" ++ show x ++ " := " ++ show y ++ " / " ++ show z
-  show (TACC Mod (Just x) (Just y) (Just z))    = "\t" ++ show x ++ " := " ++ show y ++ " % " ++ show z
+  show (ThreeAddressCode Add (Just x) (Just y) (Just z))    = "\t" ++ show x ++ " := " ++ show y ++ " + " ++ show z
+  show (ThreeAddressCode Sub (Just x) (Just y) (Just z))    = "\t" ++ show x ++ " := " ++ show y ++ " - " ++ show z
+  show (ThreeAddressCode Minus (Just x) (Just y) Nothing)   = "\t" ++ show x ++ " := -" ++ show y 
+  show (ThreeAddressCode Mult (Just x) (Just y) (Just z))   = "\t" ++ show x ++ " := " ++ show y ++ " * " ++ show z
+  show (ThreeAddressCode Div (Just x) (Just y) (Just z))    = "\t" ++ show x ++ " := " ++ show y ++ " / " ++ show z
+  show (ThreeAddressCode Mod (Just x) (Just y) (Just z))    = "\t" ++ show x ++ " := " ++ show y ++ " % " ++ show z
 -- Logical
-  show (TACC And (Just x) (Just y) (Just z))    = show x ++ " := " ++ show y ++ " && " ++ show z
-  show (TACC Or (Just x) (Just y) (Just z))     = show x ++ " := " ++ show y ++ " || " ++ show z
+  show (ThreeAddressCode And (Just x) (Just y) (Just z))    = show x ++ " := " ++ show y ++ " && " ++ show z
+  show (ThreeAddressCode Or (Just x) (Just y) (Just z))     = show x ++ " := " ++ show y ++ " || " ++ show z
 -- Comparators
-  show (TACC Gt (Just x) (Just y) (Just z))     = "\t" ++ "if " ++ show x ++ " > " ++ show y ++ " goto " ++ show z
-  show (TACC Gte (Just x) (Just y) (Just z))    = "\t" ++ "if " ++ show x ++ " >= " ++ show y ++ " goto " ++ show z
-  show (TACC Lt (Just x) (Just y) (Just z))     = "\t" ++ "if " ++ show x ++ " < " ++ show y ++ " goto " ++ show z
-  show (TACC Lte (Just x) (Just y) (Just z))    = "\t" ++ "if " ++ show x ++ " <= " ++ show y ++ " goto " ++ show z
-  show (TACC Eq (Just x) (Just y) (Just z))     = "\t" ++ "if " ++ show x ++ " == " ++ show y ++ " goto " ++ show z
-  show (TACC Neq (Just x) (Just y) (Just z))    = "\t" ++ "if " ++ show x ++ " != " ++ show y ++ " goto " ++ show z
+  show (ThreeAddressCode Gt (Just x) (Just y) (Just z))     = "\t" ++ "if " ++ show x ++ " > " ++ show y ++ " goto " ++ show z
+  show (ThreeAddressCode Gte (Just x) (Just y) (Just z))    = "\t" ++ "if " ++ show x ++ " >= " ++ show y ++ " goto " ++ show z
+  show (ThreeAddressCode Lt (Just x) (Just y) (Just z))     = "\t" ++ "if " ++ show x ++ " < " ++ show y ++ " goto " ++ show z
+  show (ThreeAddressCode Lte (Just x) (Just y) (Just z))    = "\t" ++ "if " ++ show x ++ " <= " ++ show y ++ " goto " ++ show z
+  show (ThreeAddressCode Eq (Just x) (Just y) (Just z))     = "\t" ++ "if " ++ show x ++ " == " ++ show y ++ " goto " ++ show z
+  show (ThreeAddressCode Neq (Just x) (Just y) (Just z))    = "\t" ++ "if " ++ show x ++ " != " ++ show y ++ " goto " ++ show z
 -- Jumping
-  show (TACC GoTo Nothing Nothing (Just label)) = "\t" ++ "goto " ++ show label
-  show (TACC GoTo Nothing Nothing Nothing)      = "\t" ++ "goto __"
-  show (TACC If Nothing (Just b) (Just label))  = "\t" ++ "if " ++ show b ++ " goto " ++ show label
-  show (TACC If (Just b) Nothing Nothing)       = "\t" ++ "if " ++ show b ++ " goto __"
-  show (TACC IfFalse Nothing (Just b) (Just l)) = "\t" ++ "if !" ++ show b ++ " goto " ++ show l
-  show (TACC NewLabel (Just l) Nothing Nothing) = show l ++ ": "
+  show (ThreeAddressCode GoTo Nothing Nothing (Just label)) = "\t" ++ "goto " ++ show label
+  show (ThreeAddressCode GoTo Nothing Nothing Nothing)      = "\t" ++ "goto __"
+  show (ThreeAddressCode If Nothing (Just b) (Just label))  = "\t" ++ "if " ++ show b ++ " goto " ++ show label
+  show (ThreeAddressCode If (Just b) Nothing Nothing)       = "\t" ++ "if " ++ show b ++ " goto __"
+  show (ThreeAddressCode IfFalse Nothing (Just b) (Just l)) = "\t" ++ "if !" ++ show b ++ " goto " ++ show l
+  show (ThreeAddressCode NewLabel (Just l) Nothing Nothing) = show l ++ ": "
 -- Calling functions
-  show (TACC Param Nothing (Just p) Nothing)    = "\tparam " ++ show p
-  show (TACC Call Nothing (Just f) (Just n))    = "\tcall " ++ show f ++ ", " ++ show n
-  show (TACC Call (Just x) (Just f) (Just n))   = "\t" ++ show x ++ " := call " ++ show f ++ ", " ++ show n
-  show (TACC Return Nothing Nothing Nothing)    = "\treturn" 
-  show (TACC Return Nothing (Just x) Nothing)   = "\treturn " ++ show x 
+  show (ThreeAddressCode Param Nothing (Just p) Nothing)    = "\tparam " ++ show p
+  show (ThreeAddressCode Call Nothing (Just f) (Just n))    = "\tcall " ++ show f ++ ", " ++ show n
+  show (ThreeAddressCode Call (Just x) (Just f) (Just n))   = "\t" ++ show x ++ " := call " ++ show f ++ ", " ++ show n
+  show (ThreeAddressCode Return Nothing Nothing Nothing)    = "\treturn" 
+  show (ThreeAddressCode Return Nothing (Just x) Nothing)   = "\treturn " ++ show x 
 -- Array operators
-  show (TACC Get (Just x) (Just y) (Just i))    = "\t" ++ show x ++ " := " ++ show y ++ "[" ++ show i ++ "]"
-  show (TACC Set (Just x) (Just i) (Just y))    = "\t" ++ show x ++ "[" ++ show i ++ "] := " ++ show y
-  show (TACC Anexo (Just x) (Just y) (Just z))  = "\t" ++ show x ++ " := " ++ show y ++ " : " ++ show z
-  show (TACC Concat (Just x) (Just y) (Just z)) = "\t" ++ show x ++ " := " ++ show y ++ " ++ " ++ show z
-  show (TACC Length (Just x) (Just y) _)        = "\t" ++ show x ++ " := #" ++ show y
+  show (ThreeAddressCode Get (Just x) (Just y) (Just i))    = "\t" ++ show x ++ " := " ++ show y ++ "[" ++ show i ++ "]"
+  show (ThreeAddressCode Set (Just x) (Just i) (Just y))    = "\t" ++ show x ++ "[" ++ show i ++ "] := " ++ show y
+  show (ThreeAddressCode Anexo (Just x) (Just y) (Just z))  = "\t" ++ show x ++ " := " ++ show y ++ " : " ++ show z
+  show (ThreeAddressCode Concat (Just x) (Just y) (Just z)) = "\t" ++ show x ++ " := " ++ show y ++ " ++ " ++ show z
+  show (ThreeAddressCode Length (Just x) (Just y) _)        = "\t" ++ show x ++ " := #" ++ show y
 -- Pointer operations
-  show (TACC Ref (Just x) (Just y) Nothing)     = "\t" ++ show x ++ " := &" ++ show y
-  show (TACC Deref (Just x) (Just y) Nothing)   = "\t" ++ show x ++ " := *" ++ show y
+  show (ThreeAddressCode Ref (Just x) (Just y) Nothing)     = "\t" ++ show x ++ " := &" ++ show y
+  show (ThreeAddressCode Deref (Just x) (Just y) Nothing)   = "\t" ++ show x ++ " := *" ++ show y
 -- Access for records and unions
-  show (TACC Access (Just x) (Just r) (Just f)) = "\t" ++ show x ++ " := " ++ show r ++ "." ++ show f
+  show (ThreeAddressCode Access (Just x) (Just r) (Just f)) = "\t" ++ show x ++ " := " ++ show r ++ "." ++ show f
 -- Input/Output
-  show (TACC Read Nothing (Just e) Nothing)  = "\tread " ++ show e
-  show (TACC Print Nothing (Just e) Nothing) = "\tprint " ++ show e
+  show (ThreeAddressCode Read Nothing (Just e) Nothing)  = "\tread " ++ show e
+  show (ThreeAddressCode Print Nothing (Just e) Nothing) = "\tprint " ++ show e
 -- Exit program
-  show (TACC Exit Nothing Nothing Nothing)   = "\texit"
-  show (TACC Abort Nothing Nothing Nothing)  = "\tabort"
+  show (ThreeAddressCode Exit Nothing Nothing Nothing)   = "\texit"
+  show (ThreeAddressCode Abort Nothing Nothing Nothing)  = "\tabort"
 -- Castings
-  show (TACC (Cast _ toT) (Just x) (Just y) _)  = show x ++ " := " ++ toT ++ "(" ++ show y ++ ")"
+  show (ThreeAddressCode (Cast _ toT) (Just x) (Just y) _)  = show x ++ " := " ++ toT ++ "(" ++ show y ++ ")"
 -- Operator no recognized
   show tac = show (tacLvalue tac) ++ " := " ++ show (tacRvalue1 tac) ++ " " ++ show (tacOperand tac) ++ " " ++ show (tacRvalue2 tac)
 
